8b6027c8143d1da4c6f3fe12edbf91a1057ae953
Libra <Libra@MA78SAMSUNG1505>
2015-06-14 12:37:46 +0800

    bug fix

---
+++ src/serialj_cmd/SerialJ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serialj_cmd;
+
+
+/**
+ *
+ * @author Xiaoxing
+ */
+public class SerialJ {
+
+
+    public static void main(String[] args) {
+//        PortReader p=new PortReader("COM5");
+        UI ui=new UI();
+        ui.setVisible(true);
+    }
+}
